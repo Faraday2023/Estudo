dbd3f341897f21e4596c2543d0c3f49833149a35
Marcos Paulo <marcos.paulocarneiro16@gmail.com>
2023-08-28 16:29:52 -0300

    Commit inicial do meu repositorio

---
+++ teste.py
@@ -0,0 +1,22 @@
+# Primeiro Programa em Python
+
+# Desenvolvedor: Marcos Paulo P. Carneiro
+
+print('Calculadora Básica\n')
+
+print('Menu Interativo:\n \n 1) Soma \n 2) Subtração \n 3) Divisão \n 4) Multiplicação\n')
+
+x = input('Digite a operação desejada: ')
+
+num1 = input('\nDigite o primeiro número: ')
+num2 = input('\nDigite o segundo número: ')
+
+if x == 1:
+   
+   print('\nOperação escolhida: Soma.')
+   soma = num1 + num2
+
+elif x == 2:
+   
+   print('\nOperação escolhida: Subtração.')
+   soma = num1 + num2
