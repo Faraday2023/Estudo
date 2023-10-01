513d900614e0917bd13c308b228df8c6789a4ad7
Marcos Paulo <marcos.paulocarneiro16@gmail.com>
2023-10-01 11:49:16 -0300

    Estudo sobre módulos e pacotes

---
+++ CursoEmVideo/ResumoMundo3/Módulos_e_pacotes.py
@@ -0,0 +1,11 @@
+# Modularização
+"""
+1) Dividir um programa
+2) Aumentar a legibilidade
+3) Facilitar a manutenção
+"""
+import teste # Importando módulos de outro arquivo
+
+teste.soma(2,2) # Chamando a função soma
+
+
